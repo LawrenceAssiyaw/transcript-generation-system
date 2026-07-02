339c2577042f893d209e671b7604f685b1263322
Assiyaw Lawrence <LawrenceAssiyaw@users.noreply.github.com>
2026-07-02 20:35:34 +0000

    Build scripts use temp output folder

--- run.bat
+++ run.bat
@@ -1,13 +1,13 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-if exist out rmdir /s /q out
-mkdir out
-javac -d out src\com\lawrence\transcript\*.java src\com\lawrence\transcript\exception\*.java src\com\lawrence\transcript\formatter\*.java src\com\lawrence\transcript\gui\*.java src\com\lawrence\transcript\model\*.java src\com\lawrence\transcript\service\*.java
+set "OUT_DIR=%TEMP%\transcript-generation-system-out"
+if exist "%OUT_DIR%" rmdir /s /q "%OUT_DIR%"
+mkdir "%OUT_DIR%"
+javac -d "%OUT_DIR%" src\com\lawrence\transcript\*.java src\com\lawrence\transcript\exception\*.java src\com\lawrence\transcript\formatter\*.java src\com\lawrence\transcript\gui\*.java src\com\lawrence\transcript\model\*.java src\com\lawrence\transcript\service\*.java
 if errorlevel 1 (
   echo Build failed.
   pause
   exit /b 1
 )
-java -cp out com.lawrence.transcript.Main
-
+java -cp "%OUT_DIR%" com.lawrence.transcript.Main
